b21a222f9ef2b7eedd65a1a416952aa9d8ad720f
aronrissato <aron_rissato@hotmail.com>
2018-10-09 20:20:18 -0300

    Adicinado arquivo variaveis globais/locais

---
+++ Aulas/Aula0910/Variaveis.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//Variaveis globais
+int a;
+float b;
+double c;
+
+
+int main() {
+
+	//variaveis locais
+	char d;
+	bool e;
+
+
+
+
+	system("pause");
+}
